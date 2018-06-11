6ee9222cb07425f3c1a44f95322e19a9def72774
Unknown <krokofant@users.noreply.github.com>
2018-06-11 08:49:08 +0200

    Migrate bower references

--- tasks/options/npm-contributors.js
+++ tasks/options/npm-contributors.js
@@ -1,12 +1,4 @@
 module.exports = {
-  bower: {
-    options: {
-      file: 'bower.json',
-      commitMessage: 'update bower authors',
-      filter: false,
-      as: 'authors'
-    }
-  },
   contributors: {
     options: {
       commit: false,
