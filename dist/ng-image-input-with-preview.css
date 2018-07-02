6247398bf9599cb4a6fed8931872aadee00b1b9f
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2018-07-02 11:34:28 +0300

    Release v1.0.0

--- dist/ng-image-input-with-preview.css
+++ dist/ng-image-input-with-preview.css
@@ -1,5 +1,5 @@
 /*!
- * ng-image-input-with-preview v0.1.0
+ * ng-image-input-with-preview v1.0.0
  * https://github.com/deiwin/ngImageInputWithPreview
  *
  * A FileReader based directive to easily preview and upload image files.
